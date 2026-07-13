c96ff8cff555417bc3b62231819f657efce71cc1
neko_qt <sanyekzaskyv@gmail.com>
2026-07-13 10:09:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-cd /root/myos
+cd "$(dirname "$0")"
 qemu-system-x86_64 \
     -kernel bzImage \
     -initrd rootfs.cpio.gz \
-    -append "console=ttyS0 quiet loglevel=0 root=/dev/ram0 init=/init" \
+    -append "console=ttyS0" \
     -netdev user,id=net0 -device virtio-net,netdev=net0 \
     -drive file=disk.img,format=raw,if=virtio \
-    -m 512M \
-    -display sdl
+    -m 512M -nographic
