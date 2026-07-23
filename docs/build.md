98fca5bb368427347c7d033c2a5dfaf9b6a500d1
root <root@DESKTOP-265AEIN.localdomain>
2026-07-23 13:57:04 +0000

    Clear iso branch

--- docs/build.md
+++
@@ -1,8 +0,0 @@
-## For build
-```bash
-git clone https://github.com/semen88pochuev-eng/RezzOS.git
-cd RezzOS
-./build.sh # start build script
-./start.sh # start in -nographics 
-./start-gui.sh # start in graphics 
-```
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-qemu-system-x86_64 \
-    -kernel bzImage \
-    -initrd rootfs.cpio.gz \
-    -append "console=ttyS0" \
-    -netdev user,id=net0 -device virtio-net,netdev=net0 \
-    -drive file=disk.img,format=raw,if=virtio \
-    -m 512M -nographic
